7c6dc772bdf3184a3cc3b80667e77afdbff9809a
Armine Muradyan <muradyan.armin13@gmail.com>
2024-09-09 11:29:42 +0400

    add new components and functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import BookForm from "./components/BookForm/BookForm";
+import BookList from "./components/BookList/BookList";
+import Filter from "./components/Filter/Filter";
 
 function App() {
   return (
     <div className="app">
-      <div className="app-header">
+      <header className="app-header">
         <h1>Book Library App</h1>
-      </div>
+      </header>
+      <main className="app-main">
+        <BookForm />
+        <Filter />
+        <BookList />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/BookList/BookList.js
@@ -0,0 +1,9 @@
+const BookList = () => {
+  return (
+    <div className="app-block ook-list">
+      <h2>Book List</h2>
+    </div>
+  );
+};
+
+export default BookList;
