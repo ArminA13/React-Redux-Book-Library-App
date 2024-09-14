4b853f41d65a8cd5781fb5691c81880eb7d5a4d6
Armine Muradyan <muradyan.armin13@gmail.com>
2024-09-14 15:13:24 +0400

    add redux toolkit slices for filters

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,9 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setTitleFilter } from "../../redux/slices/filterSlice";
 import "./Filter.css";
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector((state) => state.filter.title);
+
+  const handelTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+  };
   return (
     <div className="app-block filter">
-      <h2>Filters</h2>
+      <div className="filter-group">
+        <input
+          value={titleFilter}
+          type="text"
+          placeholder="Filter by Title..."
+          onChange={handelTitleFilterChange}
+        />
+      </div>
     </div>
   );
 };
